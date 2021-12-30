82a0c257a3a7ec9312a87d3098b3582ae4d477a3
Onyeukwu Chidubem <onyeukwuchidubemclinton@gmail.com>
2021-12-30 06:51:52 +0100

    Layouts for the search pages

--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recipe_book_home)
+        setContentView(R.layout.recipe_page_1)
     }
 }
