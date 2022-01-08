689375c166be0c6cf1960fc800b4f881bc3fd2a8
Onyeukwu Chidubem <onyeukwuchidubemclinton@gmail.com>
2022-01-08 08:58:47 +0100

    Added a new UI page

--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recipe_page_1)
+        setContentView(R.layout.recipe_networks)
     }
 }
