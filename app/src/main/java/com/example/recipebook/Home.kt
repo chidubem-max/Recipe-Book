6151944c66d227d2c8e02c7fda9013c88a51b1cd
Onyeukwu Chidubem <onyeukwuchidubemclinton@gmail.com>
2022-01-21 19:35:56 +0100

    Added a new UI page

--- app/src/main/java/com/example/recipebook/Home.kt
+++ app/src/main/java/com/example/recipebook/Home.kt
@@ -3,9 +3,9 @@ package com.example.recipebook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recipe_detailed)
+        setContentView(R.layout.activity_home)
     }
 }
--- app/src/main/java/com/example/recipebook/SignUp.kt
+++ app/src/main/java/com/example/recipebook/SignUp.kt
@@ -3,9 +3,9 @@ package com.example.recipebook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recipe_detailed)
+        setContentView(R.layout.activity_sign_up)
     }
 }
