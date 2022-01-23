7f49549b7c49172bd2abd67f1395df8bb9f87812
Onyeukwu Chidubem <onyeukwuchidubemclinton@gmail.com>
2022-01-23 19:55:24 +0100

    Added a new UI page

---
+++ app/src/main/java/com/example/recipebook/OpenedChats.kt
@@ -0,0 +1,11 @@
+package com.example.recipebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenedChats : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opened_chats)
+    }
+}
