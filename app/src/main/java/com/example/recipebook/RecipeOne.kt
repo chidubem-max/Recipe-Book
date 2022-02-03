1d922cad49c575da4841bc47acace09da2fb2dc5
Onyeukwu Chidubem <onyeukwuchidubemclinton@gmail.com>
2022-02-03 08:53:00 +0100

    Added a new UI page

--- app/src/main/java/com/example/recipebook/RecipeOne.kt
+++ app/src/main/java/com/example/recipebook/RecipeOne.kt
@@ -1,11 +1,18 @@
 package com.example.recipebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class RecipeOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_one)
+
+        val backBtn =findViewById<ImageButton>(R.id.back_btn)
+        backBtn.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+        }
     }
 }
